e9cb766a34fdcc283d17e0e6456395528be7036d
HenrryEsposito <52429925+HenrryEsposito@users.noreply.github.com>
2021-03-14 15:06:23 -0300

    fix: meeting date prop name

--- src/app/_models/Meeting.ts
+++ src/app/_models/Meeting.ts
@@ -6,7 +6,7 @@ import { SocialNetwork } from './SocialNetwork';
 export interface Meeting {
     id: number;
     place: string;
-    date: Date;
+    meetingDate: Date;
     theme: string;
     invited: number;
     phone: string;
